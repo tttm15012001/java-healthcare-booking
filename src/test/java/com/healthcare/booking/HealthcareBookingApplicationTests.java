bc3264e4f5856b979e5cfd4d852aeafec3d48739
namvn <nam0968024068@gmail.com>
2024-08-16 23:43:27 +0700

    Push others file used for config environment

---
+++ src/test/java/com/healthcare/booking/HealthcareBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthcare.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthcareBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
