ca2e4b113c68c5426eeb31f6daa2eeb74bf90d4d
namvn <nam0968024068@gmail.com>
2024-09-12 23:32:52 +0700

    Style + Update change tab navigation

--- src/main/resources/static/js/admin/aside-navigation.js
+++ src/main/resources/static/js/admin/aside-navigation.js
@@ -2,30 +2,32 @@ $(document).ready(function () {
     let navItems = $('#nav-list-wrapper .nav-item');
     let mainContent = $('.main-content_container');
     mainContent.addClass('show');
+    let className = mainContent.attr('data-class-page');
+    $('#nav-list-wrapper li[data-class-page="' + className + '"]').addClass('active');
 
-    navItems.on('click', 'a', function (event) {
-        event.preventDefault();
-        navItems.removeClass('active');
-        $(this).parent().toggleClass('active');
-        let url = $(this).attr('href');
-
-        $.ajax({
-            url: url,
-            type: 'GET',
-            success: function(data) {
-                window.history.pushState({}, '', url)
-                let tempDiv = $('<div></div>').html(data);
-                let newContent = tempDiv.find('.main-content_container');
-
-                // Thêm lớp "fade-in-up" cho hiệu ứng xuất hiện
-                mainContent.replaceWith(newContent);
-                mainContent = newContent;
-                setTimeout(function() {
-                    newContent.addClass('show');
-                }, 100);
-            },
-            error: function() {
-            }
-        });
-    });
+    // navItems.on('click', 'a', function (event) {
+    //     event.preventDefault();
+    //     navItems.removeClass('active');
+    //     $(this).parent().toggleClass('active');
+    //     let url = $(this).attr('href');
+    //
+    //     $.ajax({
+    //         url: url,
+    //         type: 'GET',
+    //         success: function(data) {
+    //             window.history.pushState({}, '', url)
+    //             let tempDiv = $('<div></div>').html(data);
+    //             let newContent = tempDiv.find('.main-content_container');
+    //
+    //             // Thêm lớp "fade-in-up" cho hiệu ứng xuất hiện
+    //             mainContent.replaceWith(newContent);
+    //             mainContent = newContent;
+    //             setTimeout(function() {
+    //                 newContent.addClass('show');
+    //             }, 100);
+    //         },
+    //         error: function() {
+    //         }
+    //     });
+    // });
 });
